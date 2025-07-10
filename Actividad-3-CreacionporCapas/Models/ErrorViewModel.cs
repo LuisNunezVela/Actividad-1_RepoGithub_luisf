a710f84802ab1fde702c16010ac399b54af594fc
SCPC1007 <SCPC1007@PCLAB1007>
2025-07-10 19:26:00 -0400

    Agregar archivos de proyecto.

---
+++ Actividad-3-CreacionporCapas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Actividad_3_CreacionporCapas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
